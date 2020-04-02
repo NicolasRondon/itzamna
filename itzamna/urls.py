890b3e49f421787e2bc50b9f62b1646d22302a63
Nicolas <nicolasrondon@outlook.es>
2020-04-02 02:17:32 -0500

    READY :dizzy: MVP de Usuarios, CRUD, JWT, reset password by email

--- itzamna/urls.py
+++ itzamna/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('core.urls.v1')),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/refresh/', TokenRefreshView.as_view()),
+    path('docs/', include_docs_urls(title='Academlo Tracking API', public=False)),
 ]
