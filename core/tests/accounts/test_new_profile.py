f7b139442040adf286f826045b25c30dfcdcb278
Nicolas <nicolasrondon@outlook.es>
2020-04-06 01:07:00 -0500

    docs

--- core/tests/accounts/test_new_profile.py
+++ core/tests/accounts/test_new_profile.py
@@ -6,6 +6,7 @@ class NewProfileTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user('test_user', 'test_user@mail.com', '123456')
 
+    # Test que prueba que un perfil se crea despues de crear un usuario
     def test_new_article(self):
         auth = self.client.post('/api/token/', {'email': 'test_user@mail.com', 'password': '123456'})
         token = auth.data['access']
