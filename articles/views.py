eb07495f78d912dc687b9f2658592a3588e0a95e
Nicolas <nicolasrondon@outlook.es>
2020-04-08 01:01:21 -0500

    ADD Se da permisos para que cualquiera pueda listar y recibir articulos

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 # Create your views here.
+from rest_framework.permissions import AllowAny
+
 from articles.models import Article
 from articles.serializers import ArticleSerializer, CreateArticleSerializer
 from .permissions import IsOwnerOrReadOnly
@@ -13,6 +15,9 @@ class ArticleViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action in ['update', 'partial_update', 'destroy']:
             self.permission_classes = [IsOwnerOrReadOnly]
+
+        if self.action in ['list', 'retrieve']:
+            self.permission_classes = [AllowAny]
         return [permission() for permission in self.permission_classes]
 
     def get_serializer_class(self):
